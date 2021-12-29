bc208a124b7b0ebb6c2d5ce6b95308c8175e2155
Muhammed Fasal <48950721+muhammedfasal@users.noreply.github.com>
2021-12-29 20:44:12 +0530

    Time: 106 ms (5.16%), Space: 14.5 MB (27.96%) - LeetHub

--- roman-to-integer/roman-to-integer.py
+++ roman-to-integer/roman-to-integer.py
@@ -1,11 +1,12 @@
-roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
 class Solution:
-    def romanToInt(self, S: str) -> int:
-        summ= 0
-        for i in range(len(S)-1,-1,-1):
-            num = roman[S[i]]
-            if 3*num < summ: 
-                summ = summ-num
-            else: 
-                summ = summ+num
-        return summ
+    def romanToInt(self, s: str) -> int:
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        n = len(s)
+        num = roman_map[s[n-1]]
+        for i in range(n-2,-1,-1):
+            print(roman_map[s[i]],roman_map[s[i+1]])
+            if roman_map[s[i]] >= roman_map[s[i+1]]:
+                num += roman_map[s[i]]
+            else:
+                num -= roman_map[s[i]]
+        return num
