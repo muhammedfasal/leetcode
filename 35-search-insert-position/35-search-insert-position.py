d5a137a0b38b48a2b6f3cc64bdc253a86a0c28a2
Muhammed Fasal <48950721+muhammedfasal@users.noreply.github.com>
2022-05-23 22:38:43 +0530

    Time: 38 ms (70.63%), Space: 13.9 MB (99.36%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        l , r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = ( l + r ) // 2
+            if nums[mid] == target:
+                return mid
+            if target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
+                
+                
