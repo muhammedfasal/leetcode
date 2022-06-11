7b11825fd01e999aad9816acc4f8be5711f7b6dc
Muhammed Fasal <48950721+muhammedfasal@users.noreply.github.com>
2022-06-11 23:55:54 +0530

    Time: 60 ms (19.47%), Space: 13.8 MB (64.22%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,10 +1,7 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:        
-        _counter = Counter(s)
-        output = 0
-        
-        for key, value in _counter.items():
-            output += 2*(value//2)
-            if output % 2 == 0 and value % 2 ==1:
-                output += 1
-        return output
+        c = Counter(s)
+        res = 0
+        for k, count in c.items():
+            res += count // 2
+        return res*2 + (len(s) > res*2)
