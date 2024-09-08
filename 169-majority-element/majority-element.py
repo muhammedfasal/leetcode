3c22b52cd817f35ae19ff8d1ac56af98977eb129
Muhammed Fasal <48950721+muhammedfasal@users.noreply.github.com>
2024-09-08 15:31:20 +0530

    Time: 183 ms (11.31%) | Memory: 18 MB (21.96%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dict_ = {}
-        majority = res = 0
-        for i in nums:
-            dict_[i] = 1 + dict_.get(i,0)
-            if dict_[i] > majority:
-                res = i
-                majority = dict_[i]
-        return res
+        map = {}
+        for num in nums:
+            if num in map:
+                map[num] += 1
+            else:
+                map[num] = 1
+
+        return max(map, key=map.get)
         
